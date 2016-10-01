2d432335211cc984a43b7fc1bc074bf34628d1c3
RAMONSTOHR\Ramon <ramonstohr@gmail.com>
2016-10-01 15:28:07 +0200

    2016-10-01

--- BasicConcepts2/Sources/your/app/components/Main.java
+++ BasicConcepts2/Sources/your/app/components/Main.java
@@ -1,9 +1,27 @@
 package your.app.components;
 
+import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
+import com.webobjects.foundation.NSTimestamp;
 
 public class Main extends BaseComponent {
 	public Main(WOContext context) {
 		super(context);
+	}
+	
+	public NSTimestamp systemZeit()
+	{
+		return new NSTimestamp();
+	}
+	
+	public Welcome sayHi()
+	{
+		Welcome nextPage = pageWithName(Welcome.class);
+		return nextPage;
+	}
+	
+	public WOComponent reload()
+	{
+		return null;
 	}
 }
---
+++ BasicConcepts2/Sources/your/app/components/Welcome.java
@@ -0,0 +1,10 @@
+package your.app.components;
+
+import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WOComponent;
+
+public class Welcome extends WOComponent {
+    public Welcome(WOContext context) {
+        super(context);
+    }
+}
